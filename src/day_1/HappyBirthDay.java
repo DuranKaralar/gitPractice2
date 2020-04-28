cace8d7a8429825386d81122648b81633aa0471b
DuranKaralar <64282868+DuranKaralar@users.noreply.github.com>
2020-04-28 15:50:31 -0400

    Update HappyBirthDay.java

--- src/day_1/HappyBirthDay.java
+++ src/day_1/HappyBirthDay.java
@@ -3,6 +3,8 @@ package day_1;
 public class HappyBirthDay {
     public static void main(String[] args) {
         System.out.println( "Happy BirthDay Bekir ");
+        
+        System.out.println("Kardeşşşşşş");
 
 
 
